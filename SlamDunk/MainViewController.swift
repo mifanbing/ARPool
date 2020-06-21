9a985ae5e918c174ffa9978673c5373ee3287463
zeyi wang <wang-ze-yi@hotmail.com>
2020-06-21 10:32:01 -0500

    I have walls

--- SlamDunk/MainViewController.swift
+++ SlamDunk/MainViewController.swift
@@ -3,7 +3,7 @@ import SceneKit
 import ARKit
 
 class MainViewController: UIViewController {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     var start: CGPoint?
     var end: CGPoint?
@@ -59,15 +59,15 @@ class MainViewController: UIViewController {
             let end3DTranslation = end3D.worldTransform.columns.3
             let start3DTranslation = start3D.worldTransform.columns.3
             motherBallNode.runAction(SCNAction.moveBy(x: CGFloat(end3DTranslation.x - start3DTranslation.x),
-                y: 0,
-                z: CGFloat(end3DTranslation.z - start3DTranslation.z),
-                duration: 2), forKey: "Move")
-
-//            print("start: \(startPoint.x) \(startPoint.y)")
-//            print("end: \(endPoint.x) \(endPoint.y))")
-//
-//            print("\(start3D.worldTransform.columns.3)")
-//            print("\(end3D.worldTransform.columns.3)")
+                                                      y: 0,
+                                                      z: CGFloat(end3DTranslation.z - start3DTranslation.z),
+                                                      duration: 2), forKey: "Move")
+            
+            //            print("start: \(startPoint.x) \(startPoint.y)")
+            //            print("end: \(endPoint.x) \(endPoint.y))")
+            //
+            //            print("\(start3D.worldTransform.columns.3)")
+            //            print("\(end3D.worldTransform.columns.3)")
         }
         
     }
@@ -89,18 +89,88 @@ extension MainViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
         
+        setupPlane(planeAnchor: planeAnchor, node: node)
+        setupMotherBall(planeAnchor: planeAnchor, node: node)
+        //setupTargetBall(planeAnchor: planeAnchor, node: node)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+}
+
+extension MainViewController {
+    private func setupPlane(planeAnchor: ARPlaneAnchor, node: SCNNode) {
         let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
         let planeMaterial = SCNMaterial()
         planeMaterial.diffuse.contents = UIColor.green
         plane.materials = [planeMaterial]
         
         let planeNode = SCNNode()
-        planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
         planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+        planeNode.position = SCNVector3(planeAnchor.center.x,
+                                        0,
+                                        planeAnchor.center.z)
         planeNode.geometry = plane
         
         node.addChildNode(planeNode)
         
+        let wallNegtiveZ = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(ballRadius))
+        let wallNegativeZMaterial = SCNMaterial()
+        wallNegativeZMaterial.diffuse.contents = UIColor.blue
+        wallNegtiveZ.materials = [wallNegativeZMaterial]
+        
+        let wallNegativeZNode = SCNNode()
+        wallNegativeZNode.position = SCNVector3(planeAnchor.center.x,
+                                                ballRadius / 2,
+                                                planeAnchor.center.z - planeAnchor.extent.z / 2)
+        wallNegativeZNode.geometry = wallNegtiveZ
+        
+        node.addChildNode(wallNegativeZNode)
+        
+        let wallPositiveZ = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(ballRadius))
+        let wallPositiveZMaterial = SCNMaterial()
+        wallPositiveZMaterial.diffuse.contents = UIColor.blue
+        wallPositiveZ.materials = [wallNegativeZMaterial]
+        
+        let wallPositiveZNode = SCNNode()
+        wallPositiveZNode.position = SCNVector3(planeAnchor.center.x,
+                                                ballRadius / 2,
+                                                planeAnchor.center.z + planeAnchor.extent.z / 2)
+        wallPositiveZNode.geometry = wallPositiveZ
+        
+        node.addChildNode(wallPositiveZNode)
+        
+        let wallNegtiveX = SCNPlane(width: CGFloat(planeAnchor.extent.z), height: CGFloat(ballRadius))
+        let wallNegativeXMaterial = SCNMaterial()
+        wallNegativeXMaterial.diffuse.contents = UIColor.blue
+        wallNegtiveX.materials = [wallNegativeXMaterial]
+        
+        let wallNegativeXNode = SCNNode()
+        wallNegativeXNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 0, 1, 0)
+        wallNegativeXNode.position = SCNVector3(planeAnchor.center.x - planeAnchor.extent.x / 2,
+                                                ballRadius / 2,
+                                                planeAnchor.center.z)
+        
+        wallNegativeXNode.geometry = wallNegtiveX
+        
+        node.addChildNode(wallNegativeXNode)
+        
+        let wallPositiveX = SCNPlane(width: CGFloat(planeAnchor.extent.z), height: CGFloat(ballRadius))
+        let wallPositiveXMaterial = SCNMaterial()
+        wallPositiveXMaterial.diffuse.contents = UIColor.blue
+        wallPositiveX.materials = [wallPositiveXMaterial]
+        
+        let wallPositiveXNode = SCNNode()
+        wallPositiveXNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 0, 1, 0)
+        wallPositiveXNode.position = SCNVector3(planeAnchor.center.x + planeAnchor.extent.x / 2,
+                                                ballRadius / 2,
+                                                planeAnchor.center.z)
+        wallPositiveXNode.geometry = wallPositiveX
+        
+        node.addChildNode(wallPositiveXNode)
+    }
+    
+    private func setupMotherBall(planeAnchor: ARPlaneAnchor, node: SCNNode) {
         let motherBall = SCNSphere(radius: CGFloat(ballRadius))
         let ballMaterial = SCNMaterial()
         ballMaterial.diffuse.contents = UIColor.red
@@ -115,7 +185,9 @@ extension MainViewController: ARSCNViewDelegate {
         motherBallNode.name = "mother"
         
         node.addChildNode(motherBallNode)
-        
+    }
+    
+    private func setupTargetBall(planeAnchor: ARPlaneAnchor, node: SCNNode) {
         let targetBall = SCNSphere(radius: CGFloat(ballRadius))
         let targetBallMaterial = SCNMaterial()
         targetBallMaterial.diffuse.contents = UIColor.yellow
@@ -130,9 +202,6 @@ extension MainViewController: ARSCNViewDelegate {
         targetBallNode.name = "target"
         
         node.addChildNode(targetBallNode)
-        
-        let configuration = ARWorldTrackingConfiguration()
-        sceneView.session.run(configuration)
     }
 }
 
